ec38220b6a5521d76b8e56fc08e4937794612010
acrocephalus <dani.valverde@acrocephalus.net>
2015-07-29 19:38:41 +0200

    Working on the nodes file?

--- TweetViz.py
+++ TweetViz.py
@@ -1,8 +1,8 @@
  #!/usr/bin/python
  # -*- coding: utf-8 -*-
 import tweepy
-import time
 import pandas as pd
+from datetime import datetime
 #insert your Twitter keys here
 consumer_key ='Gfo2favWdKJLOHsEh1F3AXPyl'
 consumer_secret='POuONd8rv6f33d8g8Z1WF4tEIRBI4CXyXVp9ngS6HvOeQJFB8u'
@@ -18,32 +18,37 @@ if(api.verify_credentials):
 
 
 #Create a data frame where to store each user's data
-columns = ['Username','Follower']
-df = pd.DataFrame(columns=columns)
+output = pd.DataFrame(columns=['Username','Follower','Time'])
+output.index.name = 'Id'
+
+#Create a data frame where to store nodes data
+nodes = pd.DataFrame(columns=['Nodes','Label'])
+nodes.index.name = 'Id'
 
 #Set starting Twitter handle
 ids = ['moixera']
 
 #Set depth
-depth = 3
-
-with open('output.csv', 'a') as f:
-    while depth > 0:
-        for i in range(len(ids)):
-            username = ids[i]
-    #Check if username hs been already processed
-            if username in df.Username.values:
-                continue
-            else:
-    #Start extracting each level followers
-                for user in tweepy.Cursor(api.followers, screen_name=username).items():
-                    ids.append(str(user.screen_name))
-                          
-    #Add data to the data frame
+depth = 1
+with open('nodes.csv', 'a') as n:
+    with open('output.csv', 'a') as f:
+    #Initialize output file            
+        output.to_csv(f, mode='a')
+        while depth > 0:
             for i in range(len(ids)):
-                df.loc[i] = [username,ids[i]]
+                username = ids[i]
+    #Check if username has been already processed
+                if username in output.Username.values:
+                    continue
+                else:
+    #Start extracting each level followers
+                    for user in tweepy.Cursor(api.followers, screen_name=username).items():
+                        ids.append(str(user.screen_name))                                  
+    #Add data to the data frame
+                for i in range(len(ids)):
+                    t = datetime.now()
+                    output.loc[i] = [username,ids[i],("%s-%s-%s %s:%s:%s" % (t.year,t.month,t.day, t.hour, t.month, t.second))]
+            output.to_csv(f, mode='a', header=False)
             depth = depth-1
-            df.to_csv(f, mode='a', header=False)
 f.close()
-print ids
-print df
+n.close()
