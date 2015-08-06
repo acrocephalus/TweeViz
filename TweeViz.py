fae6515270907f665f9313e597d826cfd997f810
acrocephalus <dani.valverde@acrocephalus.net>
2015-08-06 19:02:29 +0200

    Improved efficiency

--- TweeViz.py
+++ TweeViz.py
@@ -6,9 +6,8 @@ from datetime import datetime
 import TwitterLogin
 import time
 import config
-from tweetools import grouper, add_to_file
+from tweetools import grouper
 from setproctitle import setproctitle
-from textwrap import dedent as dedent
  
 #Set process name
 setproctitle('TweeViz')   
@@ -33,24 +32,26 @@ e.close
 #Set starting Twitter handle
 user = [raw_input('Username: ')]
 #Set depth
-depth = int(raw_input('Depth (defaults to 1): ') or '1')
-#Set username lookup method
+
 is_valid = 0
 while not is_valid:
     try:
-        ask_input = '''\
-        Please choose the method to be used on the user lookup calls:\n\
-        [1] Secure\n\
-        [2] Fast'''
-        print dedent(ask_input)
-        method = int(raw_input('Enter an integer corresponding to your choice [1,2]: '))
+        depth = int(raw_input('Depth (defaults to 1): ') or '1')
+        int(depth)
         is_valid = 1
-    except ValueError, e :
+    except ValueError, e:
         print ("'%s' is not a valid integer." % e.args[0].split(": ")[1])
-if method == 1:
-    method = 'Secure'
-else:
-    method = 'Fast'
+#Set batch items number
+is_valid = 0
+while not is_valid:
+    try:
+        batch_items = int(raw_input('Batch items [1-100] (Defaults to 100): ') or '100')
+        is_valid = 1
+        while batch_items < 1 or batch_items > 100:
+            print 'Integer outside allowed range. Please enter a vualue between 1-100' 
+            batch_items = int(raw_input('Batch items [1-100] (Defaults to 100): ') or '100')           
+    except ValueError, e:
+        print ("'%s' is not a valid integer." % e.args[0].split(": ")[1])
 while depth > 0:
     #Iterate over the followers list
     for i in range(len(user)):
@@ -59,76 +60,47 @@ while depth > 0:
         if username in output.Username.values:
             continue
         else:
-            remaining_id_calls = 15
-            endtime = time.time() + 15*60        
             #Start extracting followers' IDs
             print '#Retrieving users\' ids \n'
             for page in tweepy.Cursor(config.api.followers_ids, screen_name=username).pages():
-                #Check if there are IDs calls left not to hit the rate limit
-                if remaining_id_calls > 0:
-                    ids = []
-                    ids.append(page)
-                    #Getting followers' followers                    
-                    groups = grouper(ids[0],100)
-                    #Iterate over to IDs list to get each username                
-                    for u in range(len(ids)):
-                        print '#Retrieving users\' followers\n'
-                        followers = []
-                        remaining_lookup_calls = 60
-                        endtime = time.time() + 15*60
-                        #Iterate over the IDs list in batches of 100 Ids 
-                        for t in range(len(groups)):
-                            batch = groups[t]
-                            #Check if there are user lookup calls left within the 15 minutes time window
-                            if method == 'Secure':
-                                for z in range(len(batch)):
-                                    if remaining_lookup_calls > 0:
-                                        id_lookup = [str(batch[z])]
-                                        try:
-                                            ids2 = config.api.lookup_users(user_ids = id_lookup)
-                                            followers.append([str(u.screen_name) for u in ids2])
-                                            #Reset the remaining user lookup calls counter
-                                            remaining_lookup_calls = remaining_lookup_calls - 1
-                                        except:
-                                            print 'The user has changed, moved or deleted his/her account. He/She may also be suspended.'
-                                    else:
-                                        remaining_time = endtime - time.time()
-                                        print 'Going to sleep for ' + str(remaining_time) + ' seconds to avoid hitting the rate limits'
-                                        add_to_file()                                       
-                                        time.sleep(remaining_time)
-                                        #Reset time and calls counter
-                                        remaining_lookup_calls = 60
-                                        endtime = time.time() + 15*60                                                                                        
-                            #Get the IDs in batches of 100 IDs
-                            else:         
-                                for z in range(len(batch)):
-                                    if remaining_lookup_calls > 0:
-                                        try:                                                                                                            
-                                            ids2 = config.api.lookup_users(user_ids=[batch[z]])
-                                            followers.append([str(u.screen_name) for u in ids2])
-                                            #Reset the remaining user lookup calls counter
-                                            remaining_lookup_calls = remaining_lookup_calls - 1
-                                        except:
-                                            print 'The user has changed, moved or deleted his/her account. He/She may also be suspended.'
-                                    #When reached the maximum allowed lookup calls, go to sleep for the remaining time
-                                    else:
-                                        remaining_time = endtime - time.time()
-                                        print 'Going to sleep for ' + str(datetime.timedelta(seconds=remaining_time)) + ' seconds to avoid hitting the rate limits'                                    
-                                        add_to_file()
-                                        time.sleep(remaining_time)
-                                        #Reset time and calls counter
-                                        remaining_lookup_calls = 60
-                                        endtime = time.time() + 15*60                                                        
-                    #Reset the remaining ID calls counter
-                    remaining_id_calls = remaining_id_calls - 1
-                #When reached the maximum allowed lookup calls, go to sleep for the remaining time
-                else:
-                    remaining_time = endtime - time.time()
-                    print 'Going to sleep for ' + remaining_time + ' seconds to avoid hitting the rate limits'
-                    time.sleep(remaining_time)
-                    #Reset time and calls counter
-                    remaining_id_calls = 15
-                    endtime = time.time() + 15*60                                
+                ids = []
+                ids.append(page)
+                #Getting followers' followers                    
+                groups = grouper(ids[0],batch_items)
+                #Iterate over to IDs list to get each username                
+                for u in range(len(ids)):
+                    print '#Retrieving users\' followers\n'
+                    followers = []
+                    #Iterate over the IDs list in batches of 100 Ids
+                    for t in range(len(groups)):
+                            batch = groups[t]      
+                            try:                                                                                                            
+                                ids2 = config.api.lookup_users(user_ids=[batch])
+                                time.sleep(60)
+                                for u in ids2:
+                                    followers.append(str(u.screen_name))
+                            except:
+                                print 'The user has changed, moved or deleted his/her account. He/She may also be suspended.'                                     
+                    for i in range(len(followers)):
+                        t = datetime.now()
+                        output.loc[i] = [username,followers[i],("%s-%s-%s %s:%s:%s" % (t.year,t.month,t.day, t.hour, t.month, t.second))]    
+                    with open('output/output.csv', 'a') as o:
+                        output.to_csv(o, mode='a', header=False)
+                    o.close()                                                                   
+                    #Add data to the nodes data frame
+                    nodes.Nodes = list(pd.unique(output.Follower.ravel()))
+                    nodes.Label = list(pd.unique(output.Follower.ravel()))
+                    with open('output/nodes.csv', 'a') as n:
+                        nodes.to_csv(n, mode='a', header=False)
+                    n.close()        
+                    #Add data to the edges data frame
+                    edges.Source = output.Username
+                    edges.Target = output.Follower
+                    for i in range(len(edges.Source)):
+                        edges.loc[i]['Type'] = Type
+                        edges.loc[i]['Label'] = str(edges.Source[i]) + ' - ' + str(edges.Target[i])
+                    with open('output/edges.csv', 'a') as e:
+                        edges.to_csv(e, mode='a', header=False)                           
 #Reset users list    
     user = output.Follower.tolist()          
 #Reset depth value        
